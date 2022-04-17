57476498fdacdef42d3520639e093b936edbedc9
Antonio <don_ne@mail.ru>
2022-04-17 21:03:26 +0400

    Task10-13-15

---
+++ Task015/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.Clear();
+
+Console.Write("Введите число от 1 до 7: ");
+
+int numDay = Convert.ToInt32(Console.ReadLine());
+
+string[] days = {" 1 = понедельник", "2 = вторник", "3 = среда", "4 =четверг", "5 = пятница", "6 = суббота", "7 = воскресенье"};
+
+string result = (numDay>5)?$"{numDay} -> Выходной!!":$"{numDay} -> Не выходной!!";
+
+Console.WriteLine(result);
