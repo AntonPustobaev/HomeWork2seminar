043b89309386a5ead64025449912515c6607ab8b
Antonio <don_ne@mail.ru>
2022-04-17 20:39:19 +0400

    Tasr10-13

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -8,8 +8,8 @@ Console.WriteLine("Введите трёхзначное число: ");
 num = Convert.ToInt32(Console.ReadLine());
 
 string numS = num.ToString();
-int a1 = Convert.ToInt32(numS[0].ToString());
+//int a1 = Convert.ToInt32(numS[0].ToString()); на всякий случай
 int a2 = Convert.ToInt32(numS[1].ToString());
-int a3 = Convert.ToInt32(numS[2].ToString());
+//int a3 = Convert.ToInt32(numS[2].ToString()); на всякий случай
 
 Console.WriteLine($"второе число {a2}");
---
+++ Task013/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num/100>0)
+    {
+    string numS = num.ToString();
+    int a3 = Convert.ToInt32(numS[2].ToString());
+    System.Console.WriteLine($"Третья цифра числа {num} -> {a3}");
+    }
+else
+    {
+    System.Console.WriteLine($"У числа {num} нет третьей цифры");
+    }
