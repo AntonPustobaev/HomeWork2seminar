f67f150fc0507f4e15b623316b83f95e4b2b79c8
Antonio <don_ne@mail.ru>
2022-04-17 13:08:31 +0400

    Task10

---
+++ Task010/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+Console.Clear();
+
+int num;
+
+Console.WriteLine("Введите трёхзначное число: ");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+string numS = num.ToString();
+int a1 = Convert.ToInt32(numS[0].ToString());
+int a2 = Convert.ToInt32(numS[1].ToString());
+int a3 = Convert.ToInt32(numS[2].ToString());
+
+Console.WriteLine($"второе число {a2}");
